19bad9fb1e7cfbfbcfed02a1cf369540cbc8b3ae
Elad Ben-Israel <benisrae@amazon.com>
2019-12-17 10:56:52 +0200

    add static website content with bucket deployment

--- packages/catalog/lib/website/index.ts
+++ packages/catalog/lib/website/index.ts
@@ -3,6 +3,8 @@ import s3 = require('@aws-cdk/aws-s3');
 import { StaticWebsite } from "../util/static-website";
 import { MaterializeBucketIndex } from "../util/materialize-bucket-index";
 import { IHostedZone } from "@aws-cdk/aws-route53";
+import s3deploy = require('@aws-cdk/aws-s3-deployment');
+import path = require('path');
 
 export interface WebsiteProps {
   readonly hostedZone?: IHostedZone;
@@ -21,16 +23,25 @@ export class Website extends Construct {
     this.bucket = new s3.Bucket(this, 'Bucket');
 
     // automatically materializes all index.html files so they can be served
-    // without the file name: foo/bar/index.html => foo/bar
+    // without the file name: "foo/bar/index.html" => "foo/bar" (and also "foo/bar/")
     // (cloudfront doesn't support default index files)
     new MaterializeBucketIndex(this, 'MaterializeIndexFiles', {
       bucket: this.bucket
     });
 
+    const homePrefix = 'website/';
+
     new StaticWebsite(this, 'StaticWebsite', {
       bucket: this.bucket,
       hostedZone: props.hostedZone,
-      domainName: props.domainName
+      domainName: props.domainName,
+      indexFile: `${homePrefix}index.html`,
+    });
+
+    new s3deploy.BucketDeployment(this, 'DeployWebsite', {
+      destinationBucket: this.bucket,
+      destinationKeyPrefix: homePrefix,
+      sources: [ s3deploy.Source.asset(path.join(__dirname, '..', '..', 'website')) ],
     });
   }
 }
