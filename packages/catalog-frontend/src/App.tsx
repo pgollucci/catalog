5c12c2f599cf51cb0cef4cab0a723f5cba4e6616
Michael Moussa <michael.moussa@gmail.com>
2020-01-21 10:39:15 -0500

    Implement "Phase 1" search (Twitter redirect)

--- packages/catalog-frontend/src/App.tsx
+++ packages/catalog-frontend/src/App.tsx
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.png';
 import './App.css';
+
+const SearchForm: React.FC = () => {
+  const [query, setQuery] = useState("")
+
+  const handleSubmit = (event: React.FormEvent) => {
+    const baseUrl = "https://twitter.com/search?src=typed_query&q="
+    const q = encodeURIComponent(
+      "(#cdk AND (" + query.split(/ /).join(' OR ') + ")) (from:awscdkio) filter:links -filter:replies"
+    )
+
+    event.preventDefault()
+    window.location.href = baseUrl + q
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text"
+        value={query}
+        placeholder="Search packages..."
+        onChange={e => setQuery(e.target.value)}
+        autoFocus />
+    </form >
+  );
+}
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
+      <section className="App-header">
         <h1>CDK Construct Catalog</h1>
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} alt="logo" />
         <p>A catalog of AWS CDK construct libraries.</p>
-      </header>
+      </section>
+
+      <section className="App-search">
+        <SearchForm></SearchForm>
+      </section>
+
+      <section className="App-disclaimer">
+        <p>This is a community project and is not supported by AWS.</p>
+      </section>
     </div>
   );
 }
